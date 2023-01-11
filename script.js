e3dd0df7ef39ea6e13ed398cdb3de53614a8355f
Shanmus4 <s4.shanmugsundar@gmail.com>
2023-01-11 12:06:56 +0530

    changed repo name

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ LottieInteractivity.create({
     mode:"scroll",
     container:'#titlediv',
         actions: [
-        {
-            visibility:[0, 1.0],
-            type: "seek",
-            frames: [0, 120],
-        },
+            {
+                visibility:[0, 0.3],
+                type: "stop",
+                frames: [0]
+            },
+            {
+                visibility: [0.3, 1.0],
+                type: "seek",
+                frames: [0, 120]
+            }
         ]
     });
