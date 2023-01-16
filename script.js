cd0166d73c06bab5d360461a18d21abe8c7f13b5
Shanmus4 <s4.shanmugsundar@gmail.com>
2023-01-16 15:07:07 +0530

    Frame 2 animation

--- script.js
+++ script.js
@@ -1,17 +1,103 @@
 LottieInteractivity.create({
-    player:'#titlelottie',
-    mode:"scroll",
-    container:'#titlediv',
-        actions: [
-            {
-                visibility:[0, 0.3],
-                type: "stop",
-                frames: [0]
-            },
-            {
-                visibility: [0.3, 1.0],
-                type: "seek",
-                frames: [0, 120]
-            }
-        ]
-    });
+  player: "#titlelottie",
+  mode: "scroll",
+  container: "#titlediv",
+  actions: [
+    {
+      visibility: [0, 0.3],
+      type: "stop",
+      frames: [0],
+    },
+    {
+      visibility: [0.3, 1.0],
+      type: "seek",
+      frames: [0, 120],
+    },
+  ],
+});
+
+LottieInteractivity.create({
+  player: "#frame2lottie",
+  mode: "scroll",
+  container: "#frame2div",
+  actions: [
+    {
+      visibility: [0, 0.2],
+      type: "stop",
+      frames: [0],
+    },
+    {
+      visibility: [0.2, 0.65],
+      type: "seek",
+      frames: [0, 72],
+    },
+  ],
+});
+
+LottieInteractivity.create({
+  player: "#frame2lottie",
+  mode: "chain",
+  container: "#frame2div",
+
+  actions: [
+    {
+      state: "click",
+      frames: [72, 84],
+      transition: "onComplete",
+    },
+    {
+      state: "click",
+      frames: [84, 96],
+      transition: "onComplete",
+    },
+    {
+      state: "click",
+      frames: [96, 108],
+      transition: "onComplete",
+    },
+    {
+      state: "click",
+      frames: [72, 84],
+      transition: "onComplete",
+    },
+    {
+      state: "click",
+      frames: [84, 96],
+      transition: "onComplete",
+    },
+    {
+      state: "click",
+      frames: [96, 108],
+      transition: "onComplete",
+    },
+    {
+      state: "click",
+      frames: [72, 84],
+      transition: "onComplete",
+    },
+    {
+      state: "click",
+      frames: [84, 96],
+      transition: "onComplete",
+    },
+    {
+      state: "click",
+      frames: [96, 108],
+      transition: "onComplete",
+    },
+    {
+      state: "click",
+      frames: [72, 84],
+      transition: "onComplete",
+    },
+    {
+      state: "click",
+      frames: [84, 96],
+      transition: "onComplete",
+    },
+    {
+      state: "click",
+      frames: [96, 108],
+    },
+  ],
+});
