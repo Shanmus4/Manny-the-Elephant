31397c21a20c602bdcee1c5d87bcf24fc3fbb318
Shanmus4 <s4.shanmugsundar@gmail.com>
2023-01-19 00:15:44 +0530

    Added Loader

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
+window.onload = function () {
+  document.getElementById("loading").style.display = "none";
+  document.getElementById("content").style.display = "block";
+};
+
 var footballsound = new Audio();
 footballsound.src = "Assets/Frame 2/Football Kick.mp3";
