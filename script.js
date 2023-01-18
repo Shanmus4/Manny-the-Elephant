63bde3a21018d1ab328591efabbbf8a3a8fd52d3
Shanmus4 <s4.shanmugsundar@gmail.com>
2023-01-19 01:18:52 +0530

    DOM event added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", (event) => {
   document.getElementById("loading").style.display = "none";
   document.getElementById("content").style.display = "block";
-};
+});
 
 var footballsound = new Audio();
 footballsound.src = "Assets/Frame 2/Football Kick.mp3";
