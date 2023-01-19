b0f8e21654cd957620a06844fed28e73b218dd52
Shanmus4 <s4.shanmugsundar@gmail.com>
2023-01-19 10:28:49 +0530

    Changed loader

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-window.onload = (event) => {
-  document.getElementById("loading").style.display = "none";
+function hideLoader() {
+  document.getElementById("loader").style.display = "none";
   document.getElementById("content").style.display = "block";
-};
+}
 
 var footballsound = new Audio();
 footballsound.src = "Assets/Frame 2/Football Kick.mp3";
