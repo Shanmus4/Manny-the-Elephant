f7690ba1fc3b7755d5f2ded3f4b90254806f69b0
Shanmus4 <s4.shanmugsundar@gmail.com>
2023-01-24 15:05:09 +0530

    000webhost

--- feedback.php
+++ feedback.php
@@ -1,34 +1,37 @@
 <?php
-
-//$servername = "jdbc:mysql://sql6.freesqldatabase.com:3306/sql12592661";
-$servername = "sql12.freesqldatabase.com";
-$username = "sql12592661";
-$password = "Cs8cPVJT6b";
-$dbname = "sql12592661";
+$servername = "localhost";
+$username = "id20194825_mannyuser";
+$password = "12345678901234567";
+$dbname = "id20194825_manny";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
 $name = $_POST['name'];
 $email = $_POST['email'];
 $feedback = $_POST['feedback'];
 
+$to = "s4.shanmugsundar@gmail.com";
+$subject = "Manny the Elephant Feedback";
+$headers = "From: " . $email;
+$txt = "You have received a feedback from" . $name . ".\n\n" . $feedback;
+mail($to, $subject, $txt, $headers);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+
 $sql = "INSERT INTO mannyfeedback (name, email, feedback) VALUES ('$name', '$email', '$feedback')";
 
-if ($conn->query($sql) === TRUE) { ?>
-    <div style=" height: 100vh;
-                              width: 100vw;
-                              display: flex;
-                              justify-content: center;
-                              align-items: center;">
-        <p style="color: rgb(143, 81, 54);
-                          font-size: 2vw;
-                          text-align: center;
-                          padding: 2vw;">
+if ($conn->query($sql) === TRUE) {
+
+
+    ?>
+    <link rel="stylesheet" href="styles.css" />
+    <div class="fullscreencaption" style="max-height: 100%">
+        <p class="f2text1">
             Thank you for the feedback!
         </p>
     </div>
