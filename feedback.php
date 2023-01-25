9800a3b117c80e8dd13650163c8665b3bbcf32c4
Shanmus4 <s4.shanmugsundar@gmail.com>
2023-01-25 15:42:28 +0530

    Working or not

--- feedback.php
+++ feedback.php
@@ -1,10 +1,5 @@
 <?php
-/*$servername = "sql12.freesqldatabase.com";
-$username = "sql12592661";
-$password = "Cs8cPVJT6b";
-$dbname = "sql12592661";*/
-
-$servername = "sql109.byetcluster.com";
+$servername = "sql109.epizy.com";
 $username = "epiz_33449757";
 $password = "O6beDg6eGmxAzBd";
 $dbname = "epiz_33449757_manny";
@@ -12,23 +7,18 @@ $dbname = "epiz_33449757_manny";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-$name = $_POST['name'];
+$name = $_POST['names'];
 $email = $_POST['email'];
 $feedback = $_POST['feedback'];
-
-/*
-$to = "s4.shanmugsundar@gmail.com";
-$subject = "Manny the Elephant Feedback";
-$headers = "From: " . $email;
-$txt = "You have received a feedback from" . $name . ".\n\n" . $feedback;
-mail($to, $subject, $txt, $headers);*/
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO mannyfeedback (name, email, feedback) VALUES ($name, $email, $feedback)";
+$sql = "INSERT INTO mannyfeedback (names, email, feedback) VALUES ('$name', '$email', '$feedback')";
 
+echo $name;
+echo "$sql";
 
 if ($conn->query($sql) === TRUE) {
     ?>
