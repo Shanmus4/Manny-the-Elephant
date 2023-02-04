e4113cb1aa9b62a991458fcadf3692e9392cb39b
Shanmus4 <s4.shanmugsundar@gmail.com>
2023-02-04 15:48:42 +0530

    8th frame

--- feedback.php
+++ feedback.php
@@ -1,39 +1,10 @@
 <?php
-function cors()
-{
-
-    // Allow from any origin
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
-        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-        // you want to allow, and if so:
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400'); // cache for 1 day
-    }
-
-    // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            // may also be using PUT, PATCH, HEAD etc
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-        exit(0);
-    }
-
-    echo "You have CORS!";
-}
-
-cors();
-
 
 $servername = "sql109.epizy.com";
 $username = "epiz_33449757";
 $password = "O6beDg6eGmxAzBd";
 $dbname = "epiz_33449757_mannyy";
+
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -53,7 +24,6 @@ if ($conn->connect_error) {
 
 $sql = "INSERT INTO mannyfeedback (names, email, feedback, ipaddr) VALUES ('$name', '$email', '$feedback', '$ipaddr' )";
 
-echo "$sql";
 
 if ($conn->query($sql) === TRUE) {
     ?>
