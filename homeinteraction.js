0bea066ce90fbf5e12aeffa0d4b2868be37ba36b
Shanmus4 <s4.shanmugsundar@gmail.com>
2023-02-06 15:19:20 +0530

    Added form in homepage

---
+++ homeinteraction.js
@@ -0,0 +1,53 @@
+//nav bar
+
+document.getElementById("discover").addEventListener("click", function () {
+  document.getElementById("discoverpage").style.display = "block";
+  document.getElementById("feedbackpage").style.display = "none";
+  //document.getElementById("feedbackpage").style.display = "block";
+  //document.getElementById("feedbackpage").style.display = "block";
+});
+
+document.getElementById("feedbacks").addEventListener("click", function () {
+  document.getElementById("discoverpage").style.display = "none";
+  document.getElementById("feedbackpage").style.display = "block";
+  //document.getElementById("feedbackpage").style.display = "block";
+  //document.getElementById("feedbackpage").style.display = "block";
+});
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  const showNavbar = (toggleId, navId, bodyId, headerId) => {
+    const toggle = document.getElementById(toggleId),
+      nav = document.getElementById(navId),
+      bodypd = document.getElementById(bodyId),
+      headerpd = document.getElementById(headerId);
+
+    // Validate that all variables exist
+    if (toggle && nav && bodypd && headerpd) {
+      toggle.addEventListener("click", () => {
+        // show navbar
+        nav.classList.toggle("show");
+        // change icon
+        toggle.classList.toggle("bi-x-lg");
+        // add padding to body
+        bodypd.classList.toggle("body-pd");
+        // add padding to header
+        headerpd.classList.toggle("body-pd");
+      });
+    }
+  };
+
+  showNavbar("header-toggle", "nav-bar", "body-pd", "header");
+
+  /*===== LINK ACTIVE =====*/
+  const linkColor = document.querySelectorAll(".nav_link");
+
+  function colorLink() {
+    if (linkColor) {
+      linkColor.forEach((l) => l.classList.remove("active"));
+      this.classList.add("active");
+    }
+  }
+  linkColor.forEach((l) => l.addEventListener("click", colorLink));
+});
+
+//homepage
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//manny page
+
 setTimeout(function () {
   document.querySelector(".loader").style.opacity = "0";
   setTimeout(function () {
@@ -54,44 +56,4 @@ f6sounddiv.addEventListener("click", function () {
   var f6audio = new Audio();
   f6audio.src = "Assets/Frame 7/animals shout.mp3";
   f6audio.play();
-});
-
-//nav bar
-
-document.addEventListener("DOMContentLoaded", function (event) {
-  const showNavbar = (toggleId, navId, bodyId, headerId) => {
-    const toggle = document.getElementById(toggleId),
-      nav = document.getElementById(navId),
-      bodypd = document.getElementById(bodyId),
-      headerpd = document.getElementById(headerId);
-
-    // Validate that all variables exist
-    if (toggle && nav && bodypd && headerpd) {
-      toggle.addEventListener("click", () => {
-        // show navbar
-        nav.classList.toggle("show");
-        // change icon
-        toggle.classList.toggle("bx-x");
-        // add padding to body
-        bodypd.classList.toggle("body-pd");
-        // add padding to header
-        headerpd.classList.toggle("body-pd");
-      });
-    }
-  };
-
-  showNavbar("header-toggle", "nav-bar", "body-pd", "header");
-
-  /*===== LINK ACTIVE =====*/
-  const linkColor = document.querySelectorAll(".nav_link");
-
-  function colorLink() {
-    if (linkColor) {
-      linkColor.forEach((l) => l.classList.remove("active"));
-      this.classList.add("active");
-    }
-  }
-  linkColor.forEach((l) => l.addEventListener("click", colorLink));
-
-  // Your code to run since DOM is loaded and ready
 });
